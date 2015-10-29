8516d058661b3c64cc318fe63a729bfbeeffbd80
margaretyjkim <margaret.yj.kim@gmail.com>
2015-10-29 14:40:09 -0400

    class Person is created and initialize method is created with name attribute

--- people.rb
+++ people.rb
@@ -1,3 +1,10 @@
+#Question 4
+class Person
+	def initialize(name)
+		@name = name
+	end
+end
+
 #Question 1
 #Question 2
 class Student
@@ -5,7 +12,7 @@ class Student
 		puts "I get it!"
 	end
 end
-
+#Question 3
 class Instruction
 	def teach
 		puts "Everything in Ruby is an Object."
